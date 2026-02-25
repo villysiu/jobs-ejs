68fb4d4785e5e806dffe688477df99c8ab0204b4
Villy Siu <villysiu@gmail.com>
2026-02-25 10:12:39 -0800

    fix flash error

--- controllers/sessionController.js
+++ controllers/sessionController.js
@@ -8,7 +8,7 @@ const registerShow = (req, res) => {
 const registerDo = async (req, res, next) => {
   if (req.body.password != req.body.password1) {
     req.flash("error", "The passwords entered do not match.");
-    return res.render("register", {  errors: flash("errors") });
+    return res.render("register", {  errors: flash("error") });
   }
   try {
     await User.create(req.body);
@@ -20,7 +20,7 @@ const registerDo = async (req, res, next) => {
     } else {
       return next(e);
     }
-    return res.render("register", {  errors: flash("errors") });
+    return res.render("register", {  errors: flash("error") });
   }
   res.redirect("/");
 };
